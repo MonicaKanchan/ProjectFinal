cf4af251e857ada460dd69c78a4e2edf278d728e
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-12-16 17:42:28 -0500

    correction

--- pages/register.php
+++ pages/register.php
@@ -29,13 +29,13 @@
  </style>
 
 <form action="index.php?page=accounts&action=register" method="post">
-    First name:<label> <input type="text" name="fname"></label><br>
-    Last name: <label><input type="text" name="lname"></label><br>
-    Email: <label><input type="text" name="email"></label><br>
-    Phone: <label><input type="text" name="phone"></label><br>
-    Birthday:<label><input type="text" name="birthday"></label><br>
-    Gender: <label><input type="text" name="gender"></label><br>
-    Password: <label><input type="password" name="password"></label><br>
+    First name:<label> <input type="text" name="fname"></label><br><br>
+    Last name: <label><input type="text" name="lname"></label><br><br>
+    Email: <label><input type="text" name="email"></label><br><br>
+    Phone: <label><input type="text" name="phone"></label><br><br>
+    Birthday:<label><input type="text" name="birthday"></label><br><br>
+    Gender: <label><input type="text" name="gender"></label><br><br>
+    Password: <label><input type="password" name="password"></label><br><br>
     <input type="submit" value="Submit form">
 </form>
 
