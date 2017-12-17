5bc7ed3a7c274a7d440cb125290282ac44249d84
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-12-17 18:10:34 -0500

    view profile

--- pages/show_account.php
+++ pages/show_account.php
@@ -31,7 +31,7 @@ print utility\htmlTable::generateTableFromOneRecord($data)
 ?>
 
 
-<style>
+<!-- <style>
  label
  {
  width: 5em;
@@ -40,19 +40,20 @@ print utility\htmlTable::generateTableFromOneRecord($data)
  margin-right: 68.0em;
  display: block;
  }
- </style>
+ </style> -->
 
 
-<form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
+<form action="index.php?page=accounts&action=edit&id=<?php echo $data->id; ?>" method="post">
 
 
-    First name:<label><input type="text" name="fname" value="<?php echo $data->fname; ?>"></label><br><br>
-    Last name: <label><input type="text" name="lname" value="<?php echo $data->lname; ?>"></label><br><br>
-    Email:<label><input type="text" name="email" value="<?php echo $data->email; ?>"></label><br><br>
-    Phone: <label><input type="text" name="phone" value="<?php echo $data->phone; ?>"></label><br><br>
-    Birthday: <label><input type="text" name="birthday" value="<?php echo $data->birthday; ?>"></label><br><br>
-    Gender: <label><input type="text" name="gender" value="<?php echo $data->gender; ?>"></label><br><br>
-    <input type="submit" value="Submit form">
+    First name:<input type="text" name="fname" value="<?php echo $data->fname; ?>" readonly><br>
+    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>" readonly><br>
+    Email:<input type="text" name="email" value="<?php echo $data->email; ?>" readonly><br>
+    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>" readonly><br>
+    Birthday: ><input type="text" name="birthday" value="<?php echo $data->birthday; ?>" readonly><br>
+    Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>" readonly><br>
+    <button type="submit"><a href="index.php?page=accounts&action=edit&id=<?php echo $data->id; ?>">Edit</a></button>
+    <!-- <input type="submit" value="Submit form"> -->
 </form>
 
 
@@ -60,6 +61,7 @@ print utility\htmlTable::generateTableFromOneRecord($data)
     <button type="submit" form="form1" value="delete">Delete</button>
 </form>
 
+<h4><a href="index.php?page=tasks&action=all">Previous Page</a></h4>
 
 <script src="js/scripts.js"></script>
 </body>
