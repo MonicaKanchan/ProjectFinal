3195538392d8aaaf3f628066112d5e5d42ad32fb
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-12-17 03:56:22 -0500

    Login finally successfull

--- controllers/accountsController.php
+++ controllers/accountsController.php
@@ -120,7 +120,7 @@ class accountsController extends http\controller
                 //forward the user to the show all todos page
                // print_r($_SESSION);
             
-                header('Location: index.php?page=tasks&action=all'.$user->id);
+                header('Location: index.php?page=tasks&action=all');
             }
 
             else
