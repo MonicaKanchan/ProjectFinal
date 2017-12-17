e95d850efcf6655b43046573d75b4688912e4fbc
monica nagendra kanchan <mk758@afsconnect1.njit.edu>
2017-12-17 04:41:10 -0500

    commands

--- controllers/homepageController.php
+++ controllers/homepageController.php
@@ -19,6 +19,9 @@ class homepageController extends http\controller
 //If there is a session then you should show the user profile link
 //the template is an HTML page with PHP inserted in it.  just put an if/else statement to check for the session and show correct links
 
+    	session_start();
+    	session_destroy();
+
 
         $templateData['site_name'] = 'MONICA';
 
